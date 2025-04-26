b9e03a9aaa3a6c5c24ca732a9638bf61a2dbb7af
Woodywood-head <stevenorama@gmail.com>
2025-04-26 17:50:41 +0100

    ammendmnts for Issue #38  - PR Request #41

---
+++ notesApp/Notes/Views/EnvironmentalScientistPage.xaml.cs
@@ -0,0 +1,26 @@
+namespace Notes.Views;
+
+public partial class EnvironmentalScientistPage : ContentPage
+{
+	public EnvironmentalScientistPage()
+	{
+		InitializeComponent();
+	}
+	/*Alert Button - Link to Met Office */
+	private async void OnWeatherStationClicked(object sender, EventArgs e)
+	{
+		// Open the weather station link
+		var weatherStationUrl = "https://weather.metoffice.gov.uk/warnings-and-advice/uk-warnings";
+		await Launcher.Default.OpenAsync(weatherStationUrl);
+	}
+	/*Maps Button - Link to Weather station - Calton hill place holder (google Maps)*/
+	    private async void OnMapViewClicked(object sender, EventArgs e)
+    {
+        // Google Maps URL for Calton Hill Weather Center (approximate coordinates)
+        var locationUrl = "https://www.google.com/maps?q=55.9556,-3.1828";
+
+        // Open the link
+        await Launcher.Default.OpenAsync(locationUrl);
+    }
+
+}
