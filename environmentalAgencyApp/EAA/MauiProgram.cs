7668136834199d4e4e71e638e1fb57889566b767
Woodywood-head <stevenorama@gmail.com>
2025-05-03 23:42:20 +0100

    Debugging console write out - help identify connection errors empty string values for db model. Continued work

--- environmentalAgencyApp/EAA/MauiProgram.cs
+++ environmentalAgencyApp/EAA/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
@@ -9,43 +8,74 @@ using EAA.Views;
 
 namespace EAA;
 
- public static class MauiProgram
- {
- 	public static MauiApp CreateMauiApp()
- 	{
- 		var builder = MauiApp.CreateBuilder();
- 		builder
- 			.UseMauiApp<App>()
- 			.ConfigureFonts(fonts =>
- 			{
- 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
- 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
- 			});
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
 
- 		var a = Assembly.GetExecutingAssembly();
- 		using var stream = a.GetManifestResourceStream("EAA.appsettings.json");
+		var assembly = Assembly.GetExecutingAssembly();
+		var resourceName = assembly
+			.GetManifestResourceNames()
+			.FirstOrDefault(name => name.EndsWith("appsettings.json", StringComparison.OrdinalIgnoreCase));
 
- 		var config = new ConfigurationBuilder()
- 			.AddJsonStream(stream)
- 			.Build();
+		IConfigurationRoot? config = null;
+		string? connectionString = null;
 
- 		builder.Configuration.AddConfiguration(config);
+		if (resourceName is not null)
+		{
+			using var stream = assembly.GetManifestResourceStream(resourceName);
+			if (stream is not null)
+			{
+				config = new ConfigurationBuilder()
+					.AddJsonStream(stream)
+					.Build();
 
- 		var connectionString = builder.Configuration.GetConnectionString("DevelopmentConnection");
-		builder.Services.AddDbContext<NotesDbContext>(options => options.UseSqlServer(connectionString));
+				connectionString = config.GetConnectionString("DevelopmentConnection");
+			}
+			else
+			{
+				Console.WriteLine("⚠️ appsettings.json found in resources but could not be read."); // added for debuging 
+			}
+		}
+		else
+		{
+			Console.WriteLine("⚠️ appsettings.json not found in embedded resources."); // added for debuging
+		}
+
+		if (config is not null && connectionString is not null)
+		{
+			builder.Configuration.AddConfiguration(config);
+			builder.Services.AddDbContext<NotesDbContext>(options => options.UseSqlServer(connectionString));
+		}
+
 		builder.Services.AddSingleton<AllNotesViewModel>();
- 		builder.Services.AddTransient<NoteViewModel>();
- 		builder.Services.AddSingleton<AllNotesPage>();
- 		builder.Services.AddTransient<NotePage>();
-
+		builder.Services.AddTransient<NoteViewModel>();
+		builder.Services.AddSingleton<AllNotesPage>();
+		builder.Services.AddTransient<NotePage>();
 		builder.Services.AddTransient<DataPage>();
 
- #if DEBUG
- 		builder.Logging.AddDebug();
- #endif
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+		try
+		{
+			var app = builder.Build();
+			ServiceHelper.ServiceProvider = app.Services;
+			return app;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("❌ Failed to build app: " + ex);
+			throw;
+		}
 
- 		var app = builder.Build();
-    	ServiceHelper.ServiceProvider = app.Services;
-    	return app;
- 	}
- }
+	}
+}
--- environmentalAgencyApp/EAA/ViewModels/AboutViewModel.cs
+++ environmentalAgencyApp/EAA/ViewModels/AboutViewModel.cs
@@ -7,7 +7,7 @@ internal class AboutViewModel
 {
     public string Title => AppInfo.Name;
     public string Version => AppInfo.VersionString;
-    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string MoreInfoUrl => "https://github.com/Software-Engineering-Group-3-2025/SET09102_CW";
     public string Message => "This app is written in XAML and C# with .NET MAUI.";
     public ICommand ShowMoreInfoCommand { get; }
     
