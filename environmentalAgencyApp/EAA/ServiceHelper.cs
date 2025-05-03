619bb8a62241a1a632c96c5bc4c6ea87b99428cb
Timothy Graham <tomgus1.2@gmail.com>
2025-05-03 10:28:43 +0100

    Refactor: Changed the name of the app from Notes to Environmental Agency App EAA

---
+++ environmentalAgencyApp/EAA/ServiceHelper.cs
@@ -0,0 +1,10 @@
+namespace EAA
+{
+    public static class ServiceHelper
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        public static T GetService<T>() where T : class
+            => ServiceProvider.GetService(typeof(T)) as T;
+    }
+}
